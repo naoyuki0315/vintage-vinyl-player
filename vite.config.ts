be059579d883ae16fd83c0d3ff3144797d5abaa6
naoyuki0315 <suzukinaoyuki@suzukinaoyuki-no-MacBook-Air.local>
2026-05-03 09:31:20 +0900

    Fix CSP via vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'self' https://js.stripe.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com; frame-src https://js.stripe.com https://hooks.stripe.com; connect-src 'self' https://api.stripe.com;",
+    },
+  },
 })
